022a16a5368a624d487e466c4a8c27e6a0897c60
Luiz_Alexandre21 <luizalexandre21@outlook.com>
2022-03-30 11:38:26 -0300

    Update airflow_etl

--- pyetl/airflow_etl.py
+++ pyetl/airflow_etl.py
@@ -7,7 +7,7 @@ from airflow.operators.bash_operator import BashOperator
 default_args = {
    'owner': 'Luiz Alexandre Moreira',
    'depends_on_past': False,
-   'start_date': datetime.now(),
+   'start_date': timedelta(days=1),
    'retries': 0,
    }
 
@@ -15,8 +15,20 @@ default_args = {
 with DAG('my-first-dag',schedule_interval=timedelta(days=1),catchup=False,default_args=default_args) as dag:
     t1 = BashOperator(
         task_id='first_etl',
-        bash_command=""" cd $AIRFLOW_HOME/dags/etl_scripts/python3 ETL.py"""
+        bash_command=""" cd $AIRFLOW_HOME/dags/etl_scripts/python3 ETL_2019.py"""
+        )
+
+    t2 = BashOperator(
+        task_id='second_etl',
+        bash_command=""" cd $AIRFLOW_HOME/dags/etl_scripts/python3 ETL_2020.py"""
+        )
+
+    t3 = BashOperator(
+        task_id='third_etl',
+        bash_command=""" cd $AIRFLOW_HOME/dags/etl_scripts/python3 ETL_2021.py"""
         )
 
     print(“Executed code from my Python ETL!”)
 
+    # Pipeline 
+    t1 >> t2 >> t3
