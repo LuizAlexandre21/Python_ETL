05df13522a56022d994f4b44c2cd244dca13a273
Luiz_Alexandre21 <luizalexandre21@outlook.com>
2022-03-30 22:24:43 -0300

    Add conection test

--- pyetl/ETL.py
+++ pyetl/ETL.py
@@ -8,13 +8,25 @@ database_2019 = extract(2019)
 database_2020 = extract(2020)
 database_2021 = extract(2021)
 
-# Carregando dados para um possivel datalake 
+
 
 # Transformando os dados 
 atracacao_2019, carga_2019 = transformation(database_2019)
 atracacao_2020, carga_2020 = transformation(database_2020)
 atracacao_2021, carga_2021 = transformation(database_2021)
+
+
 # Verificando a existencia das tabelas 
+
+db_1 = Atracacao_fato.select().exists()
+if db_1 == False:
+    database.create_tables([Atracacao_fato])
+
+db_2 = Carga.Fato.select().exists()
+if db_2 == False:
+    database.create_tables([Carga_Fato])
+
+
 
 
 # Carregando os dados de atracacao
